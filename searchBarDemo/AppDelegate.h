524aa0e3b4b80e56f11c17b82d8d95361eab9593
emilyzhou <scarlettzwm@gmail.com>
2015-05-29 11:03:39 +0800

    Initial Commit

---
+++ searchBarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  searchBarDemo
+//
+//  Created by zwm on 15/5/29.
+//  Copyright (c) 2015年 zwm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ searchBarDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  searchBarDemo
+//
+//  Created by zwm on 15/5/29.
+//  Copyright (c) 2015年 zwm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
